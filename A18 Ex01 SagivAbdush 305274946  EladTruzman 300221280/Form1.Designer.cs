00c6f446721a64dcf983acbc0e903649dc11d0da
elad-PC\elad <eladtur777@gmail.com>
2017-11-01 22:58:48 +0200

    dsd

--- A18 Ex01 SagivAbdush 305274946  EladTruzman 300221280/Form1.Designer.cs
+++ A18 Ex01 SagivAbdush 305274946  EladTruzman 300221280/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.userImageProfile = new System.Windows.Forms.PictureBox();
+            this.loginBtn = new System.Windows.Forms.PictureBox();
+            this.dataGridViewMusic = new System.Windows.Forms.DataGridView();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
@@ -38,44 +38,58 @@
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxLatestPost = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.lblUserFirstName = new System.Windows.Forms.Label();
+            this.lblUserLastName = new System.Windows.Forms.Label();
+            this.lblUserBirthDate = new System.Windows.Forms.Label();
+            this.lblUserGender = new System.Windows.Forms.Label();
+            this.lblUserReligion = new System.Windows.Forms.Label();
+            this.lblUserEmail = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.userImageProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loginBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMusic)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox2
+            // userImageProfile
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Location = new System.Drawing.Point(0, 61);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(137, 162);
-            this.pictureBox2.TabIndex = 6;
-            this.pictureBox2.TabStop = false;
+            this.userImageProfile.BackColor = System.Drawing.Color.Transparent;
+            this.userImageProfile.Location = new System.Drawing.Point(16, 61);
+            this.userImageProfile.Name = "userImageProfile";
+            this.userImageProfile.Size = new System.Drawing.Size(141, 162);
+            this.userImageProfile.TabIndex = 6;
+            this.userImageProfile.TabStop = false;
+            this.userImageProfile.Click += new System.EventHandler(this.userImageProfile_Click);
             // 
-            // pictureBox1
+            // loginBtn
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::A18_Ex01_SagivAbdush_305274946__EladTruzman_300221280.Properties.Resources.facebook_signBtn;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, -9);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(194, 67);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
+            this.loginBtn.BackColor = System.Drawing.Color.Transparent;
+            this.loginBtn.Image = global::A18_Ex01_SagivAbdush_305274946__EladTruzman_300221280.Properties.Resources.facebook_signBtn;
+            this.loginBtn.Location = new System.Drawing.Point(-1, -9);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(194, 67);
+            this.loginBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.loginBtn.TabIndex = 5;
+            this.loginBtn.TabStop = false;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
             // 
-            // dataGridView1
+            // dataGridViewMusic
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(745, 256);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridViewMusic.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMusic.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewMusic.Name = "dataGridViewMusic";
+            this.dataGridViewMusic.Size = new System.Drawing.Size(745, 256);
+            this.dataGridViewMusic.TabIndex = 0;
             // 
             // tabControl1
             // 
@@ -92,7 +106,7 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.dataGridViewMusic);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -107,7 +121,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(743, 256);
+            this.tabPage2.Size = new System.Drawing.Size(745, 256);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -125,7 +139,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(743, 256);
+            this.tabPage3.Size = new System.Drawing.Size(745, 256);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "tabPage3";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -135,7 +149,7 @@
             this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(743, 256);
+            this.tabPage4.Size = new System.Drawing.Size(745, 256);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "tabPage4";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -145,17 +159,17 @@
             this.tabPage5.Location = new System.Drawing.Point(4, 22);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(743, 256);
+            this.tabPage5.Size = new System.Drawing.Size(745, 256);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "tabPage5";
             this.tabPage5.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // textBoxLatestPost
             // 
-            this.textBox1.Location = new System.Drawing.Point(335, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(247, 20);
-            this.textBox1.TabIndex = 8;
+            this.textBoxLatestPost.Location = new System.Drawing.Point(335, 14);
+            this.textBoxLatestPost.Name = "textBoxLatestPost";
+            this.textBoxLatestPost.Size = new System.Drawing.Size(247, 20);
+            this.textBoxLatestPost.TabIndex = 8;
             // 
             // button1
             // 
@@ -167,22 +181,167 @@
             this.button1.Text = "Add Post";
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // lblUserFirstName
+            // 
+            this.lblUserFirstName.AutoSize = true;
+            this.lblUserFirstName.ForeColor = System.Drawing.Color.Blue;
+            this.lblUserFirstName.Location = new System.Drawing.Point(219, 71);
+            this.lblUserFirstName.Name = "lblUserFirstName";
+            this.lblUserFirstName.Size = new System.Drawing.Size(60, 13);
+            this.lblUserFirstName.TabIndex = 10;
+            this.lblUserFirstName.Text = "First Name:";
+            this.lblUserFirstName.Visible = false;
+            // 
+            // lblUserLastName
+            // 
+            this.lblUserLastName.AutoSize = true;
+            this.lblUserLastName.ForeColor = System.Drawing.Color.Blue;
+            this.lblUserLastName.Location = new System.Drawing.Point(219, 88);
+            this.lblUserLastName.Name = "lblUserLastName";
+            this.lblUserLastName.Size = new System.Drawing.Size(61, 13);
+            this.lblUserLastName.TabIndex = 11;
+            this.lblUserLastName.Text = "Last Name:";
+            this.lblUserLastName.Visible = false;
+            // 
+            // lblUserBirthDate
+            // 
+            this.lblUserBirthDate.AutoSize = true;
+            this.lblUserBirthDate.ForeColor = System.Drawing.Color.Blue;
+            this.lblUserBirthDate.Location = new System.Drawing.Point(219, 105);
+            this.lblUserBirthDate.Name = "lblUserBirthDate";
+            this.lblUserBirthDate.Size = new System.Drawing.Size(57, 13);
+            this.lblUserBirthDate.TabIndex = 12;
+            this.lblUserBirthDate.Text = "Birth Date:";
+            this.lblUserBirthDate.Visible = false;
+            // 
+            // lblUserGender
+            // 
+            this.lblUserGender.AutoSize = true;
+            this.lblUserGender.ForeColor = System.Drawing.Color.Blue;
+            this.lblUserGender.Location = new System.Drawing.Point(219, 121);
+            this.lblUserGender.Name = "lblUserGender";
+            this.lblUserGender.Size = new System.Drawing.Size(45, 13);
+            this.lblUserGender.TabIndex = 13;
+            this.lblUserGender.Text = "Gender:";
+            this.lblUserGender.Visible = false;
+            // 
+            // lblUserReligion
+            // 
+            this.lblUserReligion.AutoSize = true;
+            this.lblUserReligion.ForeColor = System.Drawing.Color.Blue;
+            this.lblUserReligion.Location = new System.Drawing.Point(219, 137);
+            this.lblUserReligion.Name = "lblUserReligion";
+            this.lblUserReligion.Size = new System.Drawing.Size(48, 13);
+            this.lblUserReligion.TabIndex = 14;
+            this.lblUserReligion.Text = "Religion:";
+            this.lblUserReligion.Visible = false;
+            // 
+            // lblUserEmail
+            // 
+            this.lblUserEmail.AutoSize = true;
+            this.lblUserEmail.ForeColor = System.Drawing.Color.Blue;
+            this.lblUserEmail.Location = new System.Drawing.Point(219, 152);
+            this.lblUserEmail.Name = "lblUserEmail";
+            this.lblUserEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblUserEmail.TabIndex = 15;
+            this.lblUserEmail.Text = "Email:";
+            this.lblUserEmail.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(158, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "First Name:";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(159, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Last Name:";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(159, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Birth Date:";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(159, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Gender:";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(159, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Religion:";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(159, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Email:";
+            this.label6.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
             this.ClientSize = new System.Drawing.Size(747, 502);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblUserEmail);
+            this.Controls.Add(this.lblUserReligion);
+            this.Controls.Add(this.lblUserGender);
+            this.Controls.Add(this.lblUserBirthDate);
+            this.Controls.Add(this.lblUserLastName);
+            this.Controls.Add(this.lblUserFirstName);
+            this.Controls.Add(this.userImageProfile);
+            this.Controls.Add(this.loginBtn);
             this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxLatestPost);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userImageProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.loginBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMusic)).EndInit();
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
@@ -194,9 +353,9 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox userImageProfile;
+        private System.Windows.Forms.PictureBox loginBtn;
+        private System.Windows.Forms.DataGridView dataGridViewMusic;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
@@ -204,8 +363,20 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.TabPage tabPage5;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxLatestPost;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblUserFirstName;
+        private System.Windows.Forms.Label lblUserLastName;
+        private System.Windows.Forms.Label lblUserBirthDate;
+        private System.Windows.Forms.Label lblUserGender;
+        private System.Windows.Forms.Label lblUserReligion;
+        private System.Windows.Forms.Label lblUserEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- A18 Ex01 SagivAbdush 305274946  EladTruzman 300221280/Form1.cs
+++ A18 Ex01 SagivAbdush 305274946  EladTruzman 300221280/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
 
 namespace A18_Ex01_SagivAbdush_305274946__EladTruzman_300221280
 {
@@ -14,6 +16,149 @@ namespace A18_Ex01_SagivAbdush_305274946__EladTruzman_300221280
         public Form1()
         {
             InitializeComponent();
+            FacebookWrapper.FacebookService.s_CollectionLimit = 200;
+            FacebookWrapper.FacebookService.s_FbApiVersion = 2.8f;
+        }
+
+        User m_LoggedInUser;
+
+        private void initializeFaceBookLoginScreen()
+        {
+            /// Owner: design.patterns
+
+            /// Use the FacebookService.Login method to display the login form to any user who wish to use this application.
+            /// You can then save the result.AccessToken for future auto-connect to this user:
+            LoginResult result = FacebookService.Login("1450160541956417", /// (desig patter's "Design Patterns Course App 2.4" app)
+                "public_profile",
+                "user_education_history",
+                "user_birthday",
+                "user_actions.video",
+                "user_actions.news",
+                "user_actions.music",
+                "user_actions.fitness",
+                "user_actions.books",
+                "user_about_me",
+                "user_friends",
+                "publish_actions",
+                "user_events",
+                "user_games_activity",
+                //"user_groups" (This permission is only available for apps using Graph API version v2.3 or older.)
+                "user_hometown",
+                "user_likes",
+                "user_location",
+                "user_managed_groups",
+                "user_photos",
+                "user_posts",
+                "user_relationships",
+                "user_relationship_details",
+                "user_religion_politics",
+
+                //"user_status" (This permission is only available for apps using Graph API version v2.3 or older.)
+                "user_tagged_places",
+                "user_videos",
+                "user_website",
+                "user_work_history",
+                "read_custom_friendlists",
+
+                // "read_mailbox", (This permission is only available for apps using Graph API version v2.3 or older.)
+                "read_page_mailboxes",
+                // "read_stream", (This permission is only available for apps using Graph API version v2.3 or older.)
+                // "manage_notifications", (This permission is only available for apps using Graph API version v2.3 or older.)
+                "manage_pages",
+                "publish_pages",
+                "publish_actions",
+
+                "rsvp_event"
+                );
+            // These are NOT the complete list of permissions. Other permissions for example:
+            // "user_birthday", "user_education_history", "user_hometown", "user_likes","user_location","user_relationships","user_relationship_details","user_religion_politics", "user_videos", "user_website", "user_work_history", "email","read_insights","rsvp_event","manage_pages"
+            // The documentation regarding facebook login and permissions can be found here: 
+            // https://developers.facebook.com/docs/facebook-login/permissions#reference
+
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                m_LoggedInUser = result.LoggedInUser;
+                userImageProfile.LoadAsync(m_LoggedInUser.PictureNormalURL);
+                if (m_LoggedInUser.Posts.Count > 0)
+                {
+                    textBoxLatestPost.Text = m_LoggedInUser.Posts[0].Message;
+                }
+
+                loadUserDetailsLabelsToScreen();
+                fillTabs();
+                
+               // FacebookObjectCollection<Page> actions = FacebookService.GetCollection<Page>(actionType);
+               // dynamic actionsData = FacebookService.GetDynamicData(actionType);
+               // listViewUserDetails.Items = actions;
+               
+
+
+            }
+            else
+            {
+                MessageBox.Show(result.ErrorMessage);
+            }
+
+
+        }
+
+        public enum E_TabType { music = 1, television, movies, books};
+            
+        
+
+        
+
+
+        private void fillTabs()
+        {
+            string tt = E_TabType.music.ToString();
+            FacebookObjectCollection<Page> actions = FacebookService.GetCollection<Page>(tt);
+            dynamic actionsData = FacebookService.GetDynamicData(tt);
+            dataGridViewMusic.DataSource = actions;
+
+        }
+
+
+
+        private void loadUserDetailsLabelsToScreen()
+        {
+            lblUserFirstName.Text = m_LoggedInUser.FirstName;
+            lblUserLastName.Text = m_LoggedInUser.LastName;
+            lblUserBirthDate.Text = m_LoggedInUser.Birthday;
+            lblUserGender.Text = m_LoggedInUser.Gender.ToString();
+            lblUserReligion.Text = m_LoggedInUser.Religion;
+            lblUserEmail.Text = m_LoggedInUser.Email;
+
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+
+            lblUserFirstName.Visible = true;
+            lblUserLastName.Visible = true;
+            lblUserBirthDate.Visible = true;
+            lblUserEmail.Visible = true;
+            lblUserGender.Visible = true;
+            lblUserReligion.Visible = true;
+        }
+
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            initializeFaceBookLoginScreen();
+        }
+
+        private void userImageProfile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listViewUserDetails_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
